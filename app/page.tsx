7cd43c81ced3d806a98f74a9677d48599309f41f
sxeiter <sxeiter@mail.ru>
2024-05-30 12:02:39 +0300

    init project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Проект блога",
+  description: "это учебный проект блога Next.js",
+};
 
 export default function Home() {
   return (
